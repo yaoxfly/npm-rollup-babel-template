f85e3c93f1566f1a47ece76e2ed61e24ad3a283c
yaoxfly <1315892088@qq.com>
2022-12-01 19:20:38 +0800

    feat: demo 添加其他方法，测试打包

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export default "hello world!";
+export const test1 = async () => {
+  const a = () => {
+    return 1
+  }
+  const bb = await a()
+  return bb
+}
+
+export const test2 = "hello world!";
