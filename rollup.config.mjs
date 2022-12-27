4c190a3e167ab8c7e141c5957a5f68a9fa053962
yaoxfly <1315892088@qq.com>
2022-12-27 11:17:08 +0800

    chore: 添加rollup-plugin-dts插件汇总d.ts类型文件，配置dts插件输出index.d.ts文件

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,51 +1,76 @@
 import { defineConfig } from 'rollup';
 import terser from "@rollup/plugin-terser"
 import { nodeResolve } from '@rollup/plugin-node-resolve'; //rollup默认只能是绝对路径，可处理相对路径，省略引入文件的后缀名称(可省略的名称需要配置)
-import commonjs from '@rollup/plugin-commonjs'; //第三方库没有默认default,CommonJS 转为 ES6
-import externals from "rollup-plugin-node-externals";
+import commonjs from '@rollup/plugin-commonjs'; //rollup默认只支持 ES6+的模块方式，使支持引用commonjs的包
+import externals from "rollup-plugin-node-externals"; //自动将 dependencies 依赖声明为 externals
 import json from '@rollup/plugin-json';
 import { babel } from '@rollup/plugin-babel';
-export default defineConfig({
-  input: "src/index.ts",
-  output: [
-    {
-      file: './lib/cjs/index.js',
-      format: 'cjs'
-    },
-    {
-      file: './lib/esm/index.js',
-      format: 'es',
-    },
-    {
-      file: './lib/umd/index.js',
-      format: 'umd',
-      name: 'lib',
-      globals: {
-        "@babel/runtime-corejs3/helpers/asyncToGenerator": "@babel/runtime-corejs3/helpers/asyncToGenerator",
-        "@babel/runtime-corejs3/regenerator": '@babel/runtime-corejs3/regenerator'
+import dts from "rollup-plugin-dts";
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: './lib/cjs/index.js',
+        format: 'cjs',
       },
-    },
-  ],
-  plugins: [
-    commonjs({
-      include: /node_modules/
-    }),
-    nodeResolve({
-      extensions: ['.js', '.ts', '.jsx', '.tsx', '.mjs', '.cjs', '.json', '.node']
-    }),
-    externals({}),
-    json(),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-      include: 'src/**',
-      extensions: [".ts", ".js"],
-    }),
-    terser({
-      compress: {
-        drop_console: false,
-        drop_debugger: false
+      // {
+      //   dir: './lib/esm',
+      //   format: 'es',
+      //   // 只编译、不打包，按需引入
+      //   preserveModules: true,
+      // },
+      {
+        file: './lib/esm/index.js',
+        format: 'es',
+      },
+      {
+        file: './lib/umd/index.min.js',
+        format: 'umd',
+        name: 'myLib',
+        exports: 'named',
+        globals: {
+          "@babel/runtime-corejs3/helpers/asyncToGenerator": "@babel/runtime-corejs3/helpers/asyncToGenerator",
+          "@babel/runtime-corejs3/helpers/regeneratorRuntime": "@babel/runtime-corejs3/helpers/regeneratorRuntime"
+        },
+      },
+
+    ],
+
+    plugins: [
+      commonjs({
+        include: /node_modules/
+      }),
+      //要在nodeResolve之前
+      externals({}),
+      nodeResolve({
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.mjs', '.cjs', '.json', '.node']
+      }),
+      json(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'runtime',
+        include: 'src/**',
+        extensions: [".ts", ".js"],
+      }),
+      terser({
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "lib/typings/index.d.ts",
+        format: "es"
       }
-    }),
-  ],
-})
+    ],
+    plugins: [dts()],
+  },
+])
+
+
